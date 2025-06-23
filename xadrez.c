fa2d9d9aeaa9babbe7b3e240f455c86a2493e2ad
cpitainveloso <lucasdudu430@gmail.com>
2025-06-23 00:12:58 +0000

    jogo xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> 
+int main () {
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int i = 8;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+//  o bispo deve se mover 5 vezes a direita
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    for (int i = 0; i <= 4; i++)
+    {
+       printf(" bisipo movido para cima\n ^ direita->\n");
+    }
+      printf("\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+//movimentos da rainha a rainha deve mover 8 casas para esquerda
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    do {
+        printf("rainha movida para\n <-esquerda\n");
+          i++; 
+          printf("\n");
+    }while ( i <= 15);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+//fim do codigo 
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+return 0;
 }
